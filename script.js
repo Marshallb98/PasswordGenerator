a2c4ab7c1799dcdcecf574fa9fbcf09de2f8c9ae
Marshall Bertschy <basemarsh@gmail.com>
2020-10-31 17:11:11 -0500

    added variables

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// trying to use symbols that websites actually allow in passwords
+var sym = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", '\\', "]", "^", "_", "`", "{", "|", "}", "~"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt("Enter how long you want your password to be between 8-128 characters")
+
+  var upperCase = confirm("Do you want upper case letters in your password?")
+  if(upperCase){
+    password
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
